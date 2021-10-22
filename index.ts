0d912b349369ab5d8921dac0378d55e87967e429
Erdi Köse <me@erdkse.com>
2021-10-22 22:47:14 +0300

    feat: add index.ts and set package.json main to index.ts file

---
+++ index.ts
@@ -0,0 +1,5 @@
+import Main from './src/main';
+
+const main = new Main();
+
+main.print('Hello world!');
--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,4 @@ class Main {
     }
 }
 
-const main = new Main();
-
-main.print('Hello world!');
+export default Main;
